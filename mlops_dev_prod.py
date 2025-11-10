eda521afb2d2ee0518c659a0367da30540f38ba3
Inna Ivanova Ivanova <innaiivanova@gmail.com>
2025-11-10 16:59:39 +0100

    result4

--- mlops_dev_prod.py
+++ mlops_dev_prod.py
@@ -11,3 +11,9 @@ print(result)
 
 result2 = add_numbers(4, 3)
 print(result2)  # Output: 8
+
+result3 = add_numbers(5, 3)
+print(result3)  # Output: 8
+
+result4 = add_numbers(6, 3)
+print(result4)  # Output: 8
